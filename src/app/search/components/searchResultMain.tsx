34b5bc3ea210745ec387e9fa7133ac20dc5da1d9
minsoku <minsoku03@gmail.com>
2024-06-19 12:11:25 +0900

    최저가 검색 팝업 생성 및 전화, 문자 기능 추가

--- src/app/search/components/searchResultMain.tsx
+++ src/app/search/components/searchResultMain.tsx
@@ -1,31 +1,63 @@
+import {useState} from "react";
+
 export const SearchResultMain = ({data}: any) => {
-    // {
-    //     "id": 1,
-    //     "month": 1,
-    //     "price": 165000,
-    //     "order": 1,
-    //     "fitnessCenter": {
-    //     "id": 1,
-    //         "name": "피트니스연구소 인하대역점",
-    //         "profileImage": "https://file.woondoc.com/gym/cover/8ZZdyz1Ml8I5oMHEQHgA5U2rAggRUJ76_1646632958_4030693.jpg",
-    //         "shortAddr": "미추홀구 용현동",
-    //         "lat": "37.44779290",
-    //         "lng": "126.64925790",
-    //         "phoneNumber": "0328812226",
-    //         "safeNumber": "050702705895",
-    //         "address": "인천 미추홀구 독배로 309 노블레스타워 8층 피트니스 연구소"
-    // }
-    // }
+    const [popup, setPopup] = useState(false);
+    const popupHandler = () => {
+        setPopup(!popup)
+    }
+
+    const handlePhoneClick = (param: string) => {
+        window.location.href = `tel:${param}`; // 전화번호 수정
+    };
+    const handleSmsClick = (param: string) => {
+        window.location.href = `sms:${param}?body=안녕하세요 짐짝맨 웹사이트 보고 연락드립니다.`;
+    }
     return (
         <section
             className="h-full w-[60%] border-gray-400 border-[1px] rounded-xl">
+            {popup &&
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        {/*바탕 회색*/}
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        <div
+                            className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-middle shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                            <h3 className="text-lg text-center text-main font-bold">
+                                편하신 방법으로 상담하실 수 있어요!
+                            </h3>
+                            <div className="mt-2 flex justify-center">
+                                <button onClick={() => handlePhoneClick(data.fitnessCenter.safeNumber)} className="w-28 flex flex-col bg-gray-200 m-1 rounded-2xl items-center">
+                                    <img className="m-3 w-12"
+                                         src={"https://gymzzakman.s3.ap-northeast-2.amazonaws.com/public/call_image.png"}
+                                         alt={"전화상담"}/>
+                                    <span className="text-lg text-center font-bold">전화상담</span>
+                                </button>
+                                <button onClick={() => handleSmsClick(data.fitnessCenter.phoneNumber)} className="w-28 flex flex-col bg-gray-200 m-1 rounded-2xl items-center">
+                                    <img className="m-3 w-12"
+                                         src={"https://gymzzakman.s3.ap-northeast-2.amazonaws.com/public/message_image.png"}
+                                         alt={"문자상담"}/>
+                                    <span className="text-lg text-center font-bold">문자상담</span>
+                                </button>
+                            </div>
+                            <div className="mt-6 flex justify-center">
+                                <button onClick={popupHandler}
+                                        className="w-72 h-10 bg-main text-white font-bold text-lg rounded-xl">
+                                    다음에 하기
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
             <div
                 className="flex items-center h-full">
-                <img
-                    className="w-1/5 h-40 p-4 rounded-3xl"
-                    src={data.fitnessCenter.profileImage}
-                    alt={data.fitnessCenter.name}
-                />
+                <div className="">
+                    <img
+                        className="w-72 h-40 p-4 rounded-3xl"
+                        src={data.fitnessCenter.profileImage}
+                        alt={data.fitnessCenter.name}
+                    />
+                </div>
                 <div
                     className="mt-4 h-full w-full">
                     <div
@@ -51,6 +83,7 @@ export const SearchResultMain = ({data}: any) => {
                                 </b>
                             </div>
                             <button
+                                onClick={popupHandler}
                                 className="w-40 h-16 bg-main text-white text-2xl rounded-2xl font-bold ml-5">
                                 문의하기
                             </button>
