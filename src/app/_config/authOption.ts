b1beb605a0b57f80fd4f5e52882efc7972669916
minsoku <minsoku03@gmail.com>
2024-06-20 21:58:13 +0900

    secret 긴급 변경

--- src/app/_config/authOption.ts
+++ src/app/_config/authOption.ts
@@ -32,7 +32,7 @@ export const authOptions: NextAuthOptions = {
             }
         })
     ],
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: "toothless_kawaii",
     pages: {
         signIn: '/login'
     },
