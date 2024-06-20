1191b9458c687d1d493d2881ac26df83d245d23f
minsoku <minsoku03@gmail.com>
2024-06-21 02:49:00 +0900

    커뮤니티 리스트 height 수정

--- src/app/community/components/postList.tsx
+++ src/app/community/components/postList.tsx
@@ -6,9 +6,9 @@ interface PostListProps {
     fetchData: (page: number, categoryValue: string) => void;
 }
 
-export const PostList = ( {data, fetchData}: PostListProps) => {
+export const PostList = ( { data,fetchData}: PostListProps) => {
     return (
-        <div className="w-full h-screen">
+        <div className="w-full min-h-96">
             {
                 data?.map((post: PostData, key: number) => (
                     <PostListMain key={key} post={post} fetchData={fetchData} />
